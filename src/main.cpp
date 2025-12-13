a86784d7d0ee933f250825f6619b08427abc1e9e
Matti Laamanen <mattilaa78@gmail.com>
2025-12-13 11:00:54 +0200

    Add multiple file opening support on CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include "editor.h"
+#include <sys/stat.h>
+
+static bool isDirectory(const char* path)
+{
+    struct stat st;
+    if(stat(path, &st) != 0)
+        return false;
+    return S_ISDIR(st.st_mode);
+}
 
 int main(int argc, char* argv[])
 {
@@ -6,7 +15,19 @@ int main(int argc, char* argv[])
 
     if(argc >= 2)
     {
-        editor.openFile(argv[1]);
+        // If first argument is a directory, open file browser
+        if(isDirectory(argv[1]))
+        {
+            editor.openFileBrowser(argv[1]);
+        }
+        else
+        {
+            // Open all files as separate buffers
+            for(int i = 1; i < argc; ++i)
+            {
+                editor.openFile(argv[i]);
+            }
+        }
     }
 
     editor.run();
