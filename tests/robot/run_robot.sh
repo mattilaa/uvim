3e7d7e091041899b305495d64b1ed56cd727b18b
Matti Laamanen <mattilaa78@gmail.com>
2026-01-31 20:45:24 +0200

    Add option to run tests and install the binary if tests pass

--- tests/robot/run_robot.sh
+++ tests/robot/run_robot.sh
@@ -6,4 +6,8 @@ OUTPUT_DIR="$ROOT_DIR/tests/robot/output"
 
 mkdir -p "$OUTPUT_DIR"
 
-robot --outputdir "$OUTPUT_DIR" "$@"
+if [ "$#" -eq 0 ]; then
+  robot --outputdir "$OUTPUT_DIR" "$ROOT_DIR/tests/robot"
+else
+  robot --outputdir "$OUTPUT_DIR" "$@"
+fi
