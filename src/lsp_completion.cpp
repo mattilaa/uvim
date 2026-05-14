aeffa77035330a73bce80cdba0220b331933bf50
Matti Laamanen <mattilaa78@gmail.com>
2026-05-14 12:35:41 +0300

    Modernize and optimize code

--- src/lsp_completion.cpp
+++ src/lsp_completion.cpp
@@ -769,9 +769,8 @@ void Editor::requestCompletion()
 
             if(includeCtx->isSystem)
             {
-                std::vector<std::string> systemPaths;
+                static constexpr std::string_view systemPaths[] = {
 #ifdef __APPLE__
-                systemPaths = {
                     "/Applications/Xcode.app/Contents/Developer/Platforms/"
                     "MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/c++/"
                     "v1",
@@ -786,7 +785,6 @@ void Editor::requestCompletion()
                     "/Library/Developer/CommandLineTools/usr/include/c++/v1",
                 };
 #else
-                systemPaths = {
                     "/usr/include/c++/13",
                     "/usr/include/c++/12",
                     "/usr/include/c++/11",
@@ -798,7 +796,7 @@ void Editor::requestCompletion()
                 };
 #endif
 
-                for(const auto& base : systemPaths)
+                for(auto base : systemPaths)
                 {
                     appendIncludeEntries(base, includeCtx->dirPrefix,
                                          includeCtx->filePrefix, completionAll,
