187d91ca4741a85a21651a5f12ed52860496b15a
Matti Laamanen <mattilaa78@gmail.com>
2026-08-26 16:49:24 +0300

    Fix the Windows popup glyph corruption
      Cause: MSVC interpreted UTF-8 source files using the active Windows code page, corrupting box-drawing literals at
      compile time.
    
      Changes:
    
      - Enabled MSVC’s /utf-8 compilation option in CMakeLists.txt:37.
      - Added exact UTF-8 border-byte assertions in tests/real_mode_transitions_test.cpp:162.

--- tests/real_mode_transitions_test.cpp
+++ tests/real_mode_transitions_test.cpp
@@ -172,6 +172,9 @@ TEST(RealModeTransitionsTest, CommandPopupDocumentsHsAsHorizontalSplit)
 
     EXPECT_TRUE(text_utils::is_found(output.find("Split horizontally")));
     EXPECT_FALSE(text_utils::is_found(output.find("Split vertically")));
+    EXPECT_TRUE(text_utils::is_found(output.find("\xE2\x95\xAD")));
+    EXPECT_TRUE(text_utils::is_found(output.find("\xE2\x94\x80")));
+    EXPECT_TRUE(text_utils::is_found(output.find("\xE2\x95\xAE")));
 }
 
 TEST(RealModeTransitionsTest, VeOpensFileBrowserInVerticalSplit)
